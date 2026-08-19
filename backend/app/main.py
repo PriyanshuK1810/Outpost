664160d98c9cda3c6e27fe1bd97d616f6b92de39
Priyanshu Khare <priyanshukhare1810@gmail.com>
2026-08-19 20:12:35 +0530

    Add CORS Middleware, Wire Up Axios Client, Test frontend - backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import roadmap, topics, phases, weeks
 from . import models
 from .database import database_engine
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+
+)
 models.db_base.metadata.create_all(bind = database_engine)
 app.include_router(roadmap.router)
 app.include_router(phases.router)
